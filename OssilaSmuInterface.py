872c967b9e29bdefdbd67a0528456efc8a1524d0
Vikram Procter <vikram.procter@gmail.com>
2025-08-05 14:41:11 +0200

    Testing changes

--- OssilaSmuInterface.py
+++ OssilaSmuInterface.py
@@ -3,8 +3,11 @@ import time
 import math
 
 import xtralien
+import serial
+from serial.tools import list_ports
 
-com_no = 7  # USB COM port number of the connected Source Measure Unit
+
+com_no = 3  # USB COM port number of the connected Source Measure Unit
 channel = 'smu1'  # SMU channel to use
 i_range = 1  # Current range to use, see manual for details
 
@@ -28,8 +31,8 @@ N = len(wave)
 
 
 # set up graphics
-console_graph = drawing.ConsoleWave(15, wave=wave)
-console_graph.draw_axis()
+#console_graph = drawing.ConsoleWave(15, wave=wave)
+#console_graph.draw_axis()
 
 # Connect to the Source Measure Unit using USB
 with xtralien.Device(f'COM{com_no}') as SMU:
@@ -40,23 +43,25 @@ with xtralien.Device(f'COM{com_no}') as SMU:
     SMU[channel].set.enabled(True, response=0)
     time.sleep(0.05)
 
+    print(SMU.cloi.hello())
+
     #Initialise the set voltage
     loops = 0
     wave_index = 0
     # Loop through the voltages to measure
-    while loops < 5:
-        
+    while loops < 1:
+        """
         if wave_index == 0:
             console_graph.undraw_cursor(N-1)
         else:
             console_graph.undraw_cursor(wave_index-1)
-        
+        """
         # Set voltage, measure voltage and current
         voltage, current = SMU[channel].oneshot(wave[wave_index])[0]
 
         # Print measured voltage and current
         #print(f'V: {voltage} V; I: {current} A')
-        console_graph.draw_cursor(wave_index, current, voltage=voltage)
+        #console_graph.draw_cursor(wave_index, current, voltage=voltage)
 
         # Increment the set voltage
         wave_index += 1
@@ -65,7 +70,7 @@ with xtralien.Device(f'COM{com_no}') as SMU:
             loops += 1
         #time.sleep(0.05)
 
-    console_graph.release_terminal()
+    #console_graph.release_terminal()
     # Reset output voltage and turn off SMU 1
     SMU[channel].set.voltage(0, response=0)
     time.sleep(0.1)
